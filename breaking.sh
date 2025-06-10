606ac51826c0f5feb5a9f9c62d0753881684a3a3
Stas Dm <iqpirat@gmail.com>
2025-06-10 12:32:27 +0200

    feat: buf

--- breaking.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Set safe directory for Git
-git config --global --add safe.directory /app/.git
-
-# Execute the original buf-breaking command
-exec buf "$@"
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/ft-t/go-money-pb
-
-go 1.23.5
-
-require (
-	connectrpc.com/connect v1.18.1
-	google.golang.org/protobuf v1.36.5
-)
